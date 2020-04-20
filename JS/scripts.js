502241ac57e8a89b386f33f92a9824b7ab6cab67
kamalwc <carmichaelkw@gmail.com>
2020-04-19 23:25:55 -0400

    Javascript coding quiz web app

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,67 +1,347 @@
-// Once the user starts the quiz, present the first question and start the countdown timer.
-// When the user selects an answer, pause the quiz timer and let the user know if the answer right/wrong.
-// If the user is wrong, subtract penalty time from the clock.
-// After 2 seconds, proceed to the next question and resume the countdown timer.
-// If the countdown timer reaches 0 the quiz is over.
-// If the user answers all the questions the quiz is over.
-// The user's score and initials are then saved after which the high scores will be displayed.
-// Also provide a link straight to viewing the high scores page.
 
-
-// start quiz button ONLCLICK start the timer count down and show the countdown
-// at the end of the timer display PENCILS DOWN!
 var startButtonEL = document.querySelector("#start");
-var questionEl = document.querySelector("#question");
-var choicesEl = document.querySelector("#choices");
+
 var rightwrongEl = document.querySelector("#rightwrong");
+var testContentEL = document.querySelector(".testContent");
+var timeEl = document.querySelector("#time");
+var score;
+var time  = 75;
+
+
+
 
 
 var questions = [
     {
-        title:"What color is is the Sky?",
-        answer:"blue",
-        choices: ["green", "white", "black", "red", "blue"]
+        title:"What symbol do you use to terminate a line in Javascript?",
+        answer:"semi-colon",
+        choices: ["colon", "period", "comma", "none", "semi-colon"]
 
     },
     {
-        title:"1 + 1 = ?..",
-        answer:"2",
-        choices: ["4", "23", "1", "2"]
+        title:"What symbol do you use to select elements with ID's",
+        answer:"hashtag",
+        choices: ["hashtag", "curly braces", "semi-colon", "less than sign"]
     },
     {
-        title:"How many legs does a chair have?",
-        answer:"4",
-        choices:["4", "23", "1", "2"]
+        title:"Which tag is NOT a block level element",
+        answer:"<span></span>",
+        choices:["<div></div>", "<p></p>", "<span></span>", "<h1></h1>", "<h2></h2>"]
+    },
+    {
+        title:"Which attribute do you use to link to another html page?",
+        answer:"href",
+        choices:["id", "src", "class", "name", "href"]
+    },
+    {
+        title:"Which Keyword is NOT used to declare variables?",
+        answer:"int",
+        choices:["let", "var", "int", "const"]
     }
 ];
 
+var questionElm;
+var choicesElm;
+var correctOrWrongElm;
+var submitButtonElm;
+
+var i = 0;
+var timeInterval;
+var correctCount = 0;
+var scores = [];
+var score;
+
+
+var scoreList;
+var initialList;
+var tscore = document.querySelector("#tscore");
+var tinitial = document.querySelector("#tinitial");
+
+localStorage.setItem('scores', scores.toString());
 
 
 startButtonEL.addEventListener("click",function(){
 
-    questionEl.textContent = questions[i].title;
-    for( var i = 0; i < choices.length; i++){
-        var choice = document.createElement("li");
-        questionEl.appendChild(choice);
-        choice.textContent = questions[i].choice[i];
-    }
+    timeInterval = setInterval(timeUpdate, 1000);
 
-    choicesEl.addEventListener("click", function(e){
-        e.target.
-    });
-    if(questions[i].answer === e.target.value){
-        rightwrongEl.textContent = "Correct!";
-    }else{
-        rightwrongEl.textContent = "Wrong!";
+    testContentEL.innerHTML = "";
+    var p = document.createElement("p")
+    var p2 = document.createElement("p")
+    var ul = document.createElement("ul");
+    testContentEL.appendChild(p);
+    testContentEL.appendChild(ul);
+    testContentEL.appendChild(p2);
 
-    }
-    setTimeout(function(){
-        alert("success");
-    }, 2000);
+
+    p.setAttribute("id","question");
+    ul.setAttribute("id","choices");
+    p2.setAttribute("id","cw");
+
+   
+    questionElm = document.querySelector("#question");
+    choicesElm = document.querySelector("#choices");
+    correctOrWrongElm = document.querySelector("#cw");
+
+    qc(i);
 });
 
+function qc(i){
+    questionElm.textContent = questions[i].title;
+
+    for( var j = 0; j < questions[i].choices.length; j++){
+        var choice = document.createElement("li");
+        choice.textContent = questions[i].choices[j];
+        choice.setAttribute("data-index",questions[i].choices[j] ) //!!!!
+        choice.setAttribute("class","font");
+        choice.setAttribute("class", "lis");
 
 
-function countdown(){
+        choicesElm.appendChild(choice);
+    }
+    correctOrWrongElm.textContent =""
+}
+
+function timeUpdate(){
+    time--;
+    timeEl.textContent = time;
+    if(time <= 0){
+        submit();
+    }
+}
+
+function submit(){
+    testContentEL.innerHTML = "";
+    clearInterval(timeInterval);//pause time 
+    questionElm.innerHTML ="";
+    choicesElm.innerHTML = "";
+
+    var doneMsg = document.createElement("h2");
+    doneMsg.textContent = "All done";
+
+    var scoreMsg = document.createElement("p")
+    score = Math.round(( correctCount / questions.length ) * 100);
+    scoreMsg.textContent = "Your Final score is " + score;
+
+    var inputLabel = document.createElement('label')
+    inputLabel.textContent = "Enter initials: ";
+
+    var input = document.createElement('input')
+
+    var submitButton = document.createElement('a');
+    submitButton.textContent = "Submit";
+
+    input.setAttribute("id", "initials")
+    submitButton.setAttribute( "class", "viewScores");
+    submitButton.setAttribute( "class", "link");
+
+    submitButton.setAttribute( "href", "viewScores.html");
+
+    testContentEL.appendChild( doneMsg );
+    testContentEL.appendChild( scoreMsg );
+    testContentEL.appendChild( inputLabel );
+    testContentEL.appendChild( input );
+    testContentEL.appendChild( submitButton );
 
 }
+
+
+
+
+function checkAnswer(e){
+    var answer = e.target.textContent;
+
+    var answerMsg;
+    if(e.target.matches("li")){
+        if(answer === questions[i].answer){
+            correctCount++;
+            correctOrWrongElm.textContent = "Correct!";
+        }else{
+            time -= 2;
+            correctOrWrongElm.textContent = "Wrong!";
+        }
+
+    }
+}
+
+document.body.addEventListener("click",function(e){
+ 
+    if(e.target.matches("li")){
+        clearInterval(timeInterval);//pause time 
+        checkAnswer(e);// displays w or r
+        i++; //next question index
+
+        //after 2 seconds 
+        setTimeout(function(){
+            if(i === questions.length){
+                submit();
+            }else{
+                choicesElm.innerHTML ="";
+                questionElm.innerHTML ="";
+                qc(i);
+                timeInterval = setInterval(timeUpdate, 1000);
+            }
+        }, 2000)
+        showScores();
+    }
+});
+
+var existing;
+testContentEL.addEventListener("click",function(event){
+    var att = event.target.getAttribute("class");
+    var nscore = {
+        initial: event.target.value,
+        score: score
+    }
+    if(att == "viewScores"){
+        alert("submit!");
+        existing = existing ? existing.split(',') : [];
+        existing.push(nscore);
+        localStorage.setItem("scores", existing.toString())
+    }
+    existing = localStorage.getItem('scores');
+    
+    
+});
+
+function initialRender(){
+    var storedScores = JSON.parse(localStorage.getItem("scores"));
+    if(storedScores != null){
+        scores = storedScores;
+    }
+
+    renderScores();
+}
+
+function renderScores(){
+    for (let i = 0; i < scores.length; i++) {
+        scoreList = document.createElement("li");
+        scoreList.textContent = scores[i].score;
+        tscoreEl.append(scoreList);
+
+        initialList = document.createElement('p');
+        initialList.textContent = scores[i].initial;
+        tinitialEl.append(initialList);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function showScores(){
+    var scorez = localStorage.getItem("scores");
+
+    for(var i = 0; i < scorez.length; i++){
+        scoreList = document.createElement("li");
+        scoreList.textContent = scorez[i].score;
+        tscoreEl.append(scoreList);
+
+        initialList = document.createElement('p');
+        initialList.textContent = scorez[i].initial;
+        tinitialEl.append(initialList);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// add the time feature 
+
+// HIGHSCORES PAGE 
+//      lists scores of all people (local storage)
+//      go back to test to retake 
+//      clear local storage  highscores
+
+// style 
+
+
